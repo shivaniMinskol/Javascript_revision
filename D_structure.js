58f527d4cecc612396ada2ad2de3bd5cdb3bfff1
shivaniMinskole <shivaniminskole27@gmail.com>
2024-03-22 00:47:34 +0530

    added

--- D_structure.js
+++ D_structure.js
@@ -51,17 +51,17 @@ let states ={
     Rajasthan:["Udaipur","Jaipur","Jodhpur"]
 }
     
-// let {Maharashtra:[m1,p1,n1,n2],Chhattisgarh,Uttarpradesh:[l1,p2],Rajasthan:[u1,j1,j2]} = states
-// console.log(m1)
-// console.log(p1)
-// console.log(n1)
-// console.log(n2)
-// console.log(l1)
-// console.log(p2)
-// console.log(j1)
-// console.log(u1)
-// console.log(j1)
-// console.log(Chhattisgarh)
+let {Maharashtra:[m1,p1,n1,n2],Chhattisgarh:CH,Uttarpradesh:[l1,p2],Rajasthan:[u1,j1,j2]} = states
+console.log(m1)
+console.log(p1)
+console.log(n1)
+console.log(n2)
+console.log(l1)
+console.log(p2)
+console.log(j1)
+console.log(u1)
+console.log(j1)
+console.log(CH)
 
 
 
@@ -69,33 +69,33 @@ let states ={
 
 // program-5
 
-let dataObject = {
-    numbers: [1, 2, 3, 4, 5],
-    isEven: true,
-    isPositive: false,
-    fruits: ["apple", "banana", "orange"],
-    age: 25,
-    temperatures: [20.5, 21, 22.5, 23.7],
-    hasPets: true
-};
+// let dataObject = {
+//     numbers: [1, 2, 3, 4, 5],
+//     isEven: true,
+//     isPositive: false,
+//     fruits: ["apple", "banana", "orange"],
+//     age: 25,
+//     temperatures: [20.5, 21, 22.5, 23.7],
+//     hasPets: true
+// };
 
-let {numbers:[n1,n2,n3,n4,n5],isEven,isPositive,fruits:[a1,b1,o1],age,temperatures:[h1,h2,h3,h4],hasPets} = dataObject
+// let {numbers:[n1,n2,n3,n4,n5],isEven,isPositive,fruits:[a1,b1,o1],age,temperatures:[h1,h2,h3,h4],hasPets} = dataObject
 
-console.log(hasPets)
-console.log(isEven)
-console.log(isEven)
-console.log(a1)
-console.log(n1)
-console.log(n2)
-console.log(n3)
-console.log(n4)
-console.log(n5)
-console.log(b1)
-console.log(h1)
-console.log(h2)
-console.log(o1)
-console.log(age)
-console.log(h3)
-console.log(h4)
+// console.log(hasPets)
+// console.log(isEven)
+// console.log(isEven)
+// console.log(a1)
+// console.log(n1)
+// console.log(n2)
+// console.log(n3)
+// console.log(n4)
+// console.log(n5)
+// console.log(b1)
+// console.log(h1)
+// console.log(h2)
+// console.log(o1)
+// console.log(age)
+// console.log(h3)
+// console.log(h4)
 
 
