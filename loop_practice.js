41e2403a581942d3b9d327338d537f651a41969f
shivaniMinskole <shivaniminskole27@gmail.com>
2024-03-21 21:27:59 +0530

    added

---
+++ loop_practice.js
@@ -0,0 +1,61 @@
+//for loop
+//while loop
+
+// for(initialized;conditionCheck;incremenet/decrement){
+//     //statemenets
+// }
+
+// 1 - 10
+
+
+for (let i = 1; i <= 10 ; i++){
+    console.log(i)
+    
+}
+
+// 1 - 5 
+
+for (let i = 1; i <= 5; i++){
+    console.log(i)
+}
+
+// 20 - 1
+
+// for (let i = 20; i >=1; i--){
+//     console.log(i)
+// }
+
+// ------------------------------------------------------
+//while loop
+
+// initialized
+// while(condition){
+//     //statemenet
+//     //increemnet/decremenet
+// }
+let i = 20
+while(i >=1){
+    console.log(i)
+    i--
+
+}
+
+console.log("------------------------------")
+
+let j = 1
+
+while(j<=10){
+    console.log(j)
+    j++
+}
+
+console.log("------------------------------")
+
+let sumOne = 0
+let k = 1
+while (k <= 5) {
+    sumOne = sumOne + k
+    k++
+}
+
+console.log(sumOne)
