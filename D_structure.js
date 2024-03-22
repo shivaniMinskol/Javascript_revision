b4698e78c1c74a04e06784dcf05187f66761bffe
shivaniMinskole <shivaniminskole27@gmail.com>
2024-03-22 11:48:43 +0530

    added

--- D_structure.js
+++ D_structure.js
@@ -1,9 +1,9 @@
 
 // program-1
 // array store value by index
-let li = ["shivani","sarika","dipanshu","vrushali","sayali","snehal"]
+// let li = ["shivani","sarika","dipanshu","vrushali","sayali","snehal"]
 
-let [s1,s2,s3,,s5] = li
+// let [s1,s2,s3,,s5] = li
 
 // console.log(s1)
 // console.log(s2)
@@ -14,7 +14,7 @@ let [s1,s2,s3,,s5] = li
 // program-2
 // multi-dimensional array [[],[],[]]
 
-let li2 = [["Pune","Nagpur"],["Hyderabad","Chennai"],["Bangalore","Mysore"],["Varanasi","Lucknow"]]
+// let li2 = [["Pune","Nagpur"],["Hyderabad","Chennai"],["Bangalore","Mysore"],["Varanasi","Lucknow"]]
 
 // let [[p1,n2],[h1,c1],[b1,m1],[v1,l1]]= li2
 
@@ -32,36 +32,36 @@ let li2 = [["Pune","Nagpur"],["Hyderabad","Chennai"],["Bangalore","Mysore"],["Va
 // program-3
 // object store value by key-value pair
 
-let li3 = [{firstName:"Shivani",lastName:"Hedau"},{firstName:"Pranay",lastName:"Hedau"}]
+// let li3 = [{firstName:"Shivani",lastName:"Hedau"},{firstName:"Pranay",lastName:"Hedau"}]
 
-let [{firstName:sfname,lastName:slname},{firstName:pfname,lastName:plname}] = li3
+// let [{firstName:sfname,lastName:slname},{firstName:pfname,lastName:plname}] = li3
 
-console.log(`${sfname} ${slname}`)
-console.log(`${pfname} ${plname}`)
+// console.log(`${sfname} ${slname}`)
+// console.log(`${pfname} ${plname}`)
 
 // -----------------------------------------------------------------------------------------
 
 // program-4
 
-let states ={
+// let states ={
 
-    Maharashtra:["Mumbai","Pune","Nagpur","Nasik"],
-    Chhattisgarh:"Bilaspur",
-    Uttarpradesh:["lucknow","Prayagraj"],
-    Rajasthan:["Udaipur","Jaipur","Jodhpur"]
-}
+//     Maharashtra:["Mumbai","Pune","Nagpur","Nasik"],
+//     Chhattisgarh:"Bilaspur",
+//     Uttarpradesh:["lucknow","Prayagraj"],
+//     Rajasthan:["Udaipur","Jaipur","Jodhpur"]
+// }
     
-let {Maharashtra:[m1,p1,n1,n2],Chhattisgarh:CH,Uttarpradesh:[l1,p2],Rajasthan:[u1,j1,j2]} = states
-console.log(m1)
-console.log(p1)
-console.log(n1)
-console.log(n2)
-console.log(l1)
-console.log(p2)
-console.log(j1)
-console.log(u1)
-console.log(j1)
-console.log(CH)
+// let {Maharashtra:[m1,p1,n1,n2],Chhattisgarh:CH,Uttarpradesh:[l1,p2],Rajasthan:[u1,j1,j2]} = states
+// console.log(m1)
+// console.log(p1)
+// console.log(n1)
+// console.log(n2)
+// console.log(l1)
+// console.log(p2)
+// console.log(j1)
+// console.log(u1)
+// console.log(j1)
+// console.log(CH)
 
 
 
@@ -97,5 +97,46 @@ console.log(CH)
 // console.log(age)
 // console.log(h3)
 // console.log(h4)
+
+// ------------------------------------------------------------------------------------------------
+
+// program-6
+// let li4 = [["Pune","Nagpur"],["Hyderabad","Chennai"],["Bangalore","Mysore"],["Varanasi","Lucknow"]]
+
+// let [[p1,n1],[h1,c1],[b1,m1],[v1,l1]] = li4
+
+// console.log(p1)
+// console.log(n1)
+// console.log(c1)
+// console.log(b1)
+// console.log(m1)
+// console.log(v1)
+// console.log(l1)
+
+// -----------------------------------------------------------------------------------------------
+
+// program-7
+
+let states ={
+
+        Maharashtra:["Mumbai","Pune","Nagpur","Nasik"],
+        Chhattisgarh:"Bilaspur",
+        Uttarpradesh:["lucknow","Prayagraj"],
+        Rajasthan:["Udaipur","Jaipur","Jodhpur"]
+    
+}
+    
+let {Maharashtra:[m1,p1,n1,n2],Chhattisgarh,Uttarpradesh:[l1,p2],Rajasthan:[u1,j1,j2]} = states
+
+console.log(m1)
+console.log(p1)
+console.log(n1)
+console.log(n2)
+console.log(Chhattisgarh)
+console.log(l1)
+console.log(j2)
+console.log(j1)
+console.log(u1)
+
 
 
